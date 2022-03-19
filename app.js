0802b63b15d9cb0047aef68731a8d53b1525f21a
MrLijan <liramj21@gmail.com>
2022-03-19 19:13:04 +0200

    INIT

---
+++ app.js
@@ -0,0 +1,34 @@
+const editor = document.getElementById('editor');
+const terminal = document.getElementById('terminal');
+const runBtn = document.getElementById('run-btn');
+
+runBtn.addEventListener('click', async () => {
+  await runCode(editor.value)
+});
+
+
+
+// PYODIDE
+let pyodide = null
+
+
+async function load() {
+  pyodide = await loadPyodide({
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.1/full/"
+  });
+
+  return pyodide
+}
+let pyodideReadyPromise = load();
+
+// FUNCTIONALITY
+async function runCode() {
+  let pyodide = await pyodideReadyPromise;
+  try {
+    let output = await pyodide.runPython(editor.value);
+    terminal.innerText = `>>> ${output}`
+  } catch (err) {
+    console.log(err);
+  }
+
+}
